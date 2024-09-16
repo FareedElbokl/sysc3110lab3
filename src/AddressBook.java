285c6db5a170b0e75828f9175602b1ea4d476b1b
Fareed Elbokl <fareed.elbokl@gmail.com>
2024-09-16 11:19:02 -0400

    Modified main method for AddressBook

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -20,6 +20,9 @@ public class AddressBook {
     }
 
     public static void main(String[] args) {
-        System.out.println("Address book");
+        BuddyInfo buddy = new BuddyInfo("Tom", "Carleton", "613");
+        AddressBook addressBook = new AddressBook();
+        addressBook.addBuddy(buddy);
+        addressBook.removeBuddy(buddy);
     }
 }
